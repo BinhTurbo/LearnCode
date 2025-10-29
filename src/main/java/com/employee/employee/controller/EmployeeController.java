1996dd884d9a1aa6f833057643237434f0f5e8a8
binhbn <binhbn@softdreams.vn>
2025-10-29 08:10:30 +0700

    add code

---
+++ src/main/java/com/employee/employee/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.employee.employee.controller;
+
+import com.employee.employee.model.Employee;
+import com.employee.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employee
+    @GetMapping("employees")
+    public List<Employee> getAllEmployee(){
+        return employeeRepository.findAll();
+    }
+}
